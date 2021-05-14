c748472731515f7c661d541db1569bd41f67e69e
A_J <Ahmed.j.jibril@users.noreply.github.com>
2021-05-14 15:51:07 +0200

    working on the carousel slider

--- JS/home.js
+++ JS/home.js
@@ -5,15 +5,61 @@ async function getPosts() {
         
         for (let i = 0; i < result.length; i++) {
 
-            document.querySelector('.bottom-lower-content').innerHTML += `
-            <div class="">
-            <a href="/details-page.html?id=${result[i].id}"><h3>${result[i].title.rendered}</h3></a>
+            document.querySelector('.carousel').innerHTML += `
+            <div class="carousel__item carousel__item--visible">
+                <img src="/img/TrapSoul.jpg" alt="album cover">
+                <a href="/details-page.html?id=${result[i].id}">
+                    <h3>${result[i].title.rendered}</h3>
+                </a>
+            </div>
+            <div class="carousel__actions">
+                <button class="carousel__button--prev" aria-label="previous slide">
+                    <i class="fas fa-chevron-circle-left"></i>
+                </button>
+                <button class="carousel__button--next" aria-label="next slide">
+                    <i class="fas fa-chevron-circle-right"></i>
+                </button>
             </div>
             `;
-            if (i === 7) {
-                break
+        };
+        /* ---------------------------------------- Carousel slider --------------------------------------------- */
+        let slidePosition = 0;
+        const slides = document.querySelector('.carousel__item');
+        const totalSlides = slides.length;
+
+        document.querySelector('.carousel__button--prev').addEventListener("click", function() {
+            moveToPrevSlide();
+        });
+        document.querySelector('.carousel__button--next').addEventListener("click", function() {
+            moveToNextSlide();
+        });
+
+        function updateSlidePosition() {
+            for (let slide of slides) {
+                slide.classList.remove('carousel__item--visible');
+                slide.classList.add('carousel__item--hidden');
             }
+            slides[slidePosition].classList.add('carousel__item--visible');
         }
+
+        function moveToNextSlide() {
+            if (slidePosition === totalSlides - 1) {
+                slidePosition = 0;
+            } else {
+                slidePosition++;
+            }
+            updateSlidePosition();
+        }
+
+        function moveToPrevSlide() {
+            if (slidePosition === 0) {
+                slidePosition = totalSlides - 1;
+            } else {
+                slidePosition--;
+            }
+            updateSlidePosition();
+        }
+        /* ---------------------------------------- Carousel slider --------------------------------------------- */
     } catch (error) {
         document.querySelector('.alert').innerHTML += thisIsAnAlert(
             'An error has occured',
